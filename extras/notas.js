4c72351ad8cd2b05342981c74bdea3651cf1ce92
agustinpfs <agustinpfs@gmail.com>
2017-02-01 11:48:20 -0300

    add responsive menu

---
+++ extras/notas.js
@@ -0,0 +1,20 @@
+mokup
+html definición
+estructura básica
+layout full primario
+
+header layout full
+tags explain
+anidación
+
+css definición
+link hoja
+box model
+reset
+iconos
+suit
+mediaqueries
+css explain - flex - positions - etc
+google fonts
+
+
