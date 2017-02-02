7cf878b6dca15cd52f1074df55cf8c4eead5b4ab
agustinpfs <agustinpfs@gmail.com>
2017-02-02 19:40:12 -0300

    add slidershow,description and features sections

--- extras/notas.js
+++ extras/notas.js
@@ -17,4 +17,9 @@ mediaqueries
 css explain - flex - positions - etc
 google fonts
 
+sliders!!
+cambiar fotos doble (2000 por 1000) y llevar a 1000 en css hasta 1280
+luego comparar con el inspector mobile!
+
+llevar onclick a js file!!
 
