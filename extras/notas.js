a3af77ab3123f19d81e4fea89cbe3d8a6ca8b934
agustinpfs <agustinpfs@gmail.com>
2017-02-11 13:02:16 -0300

    add sections, sidebar and article page. first version

--- extras/notas.js
+++ extras/notas.js
@@ -23,3 +23,55 @@ luego comparar con el inspector mobile!
 
 llevar onclick a js file!!
 
+
+
+limit tweet
+data-tweet-limit=5 
+
+height tweet
+data-height="1100"
+
+Muy buen artículo, me ha dado la impresión que encierra mucho más que un punto de vista.
+De manera precisa, lo expuesto puede contener claves con el poder de convertirse en un
+punto de inflexión en la programación de todo desarrollador.
+Nos encontramos aturdidos con la gran cantidad de librerías y frameworks que se renuevan día
+a día, dando lugar a un permanente debate de cuál es la que más se acerca a nuestras 
+espectativas. Vanilla Javascript siempre estuvo y 
+tiene la capacidad de quitarnos la pesada mochila mental que nos lleva depender de otros 
+y limitar así nuestra libertad de programar.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.MainIndex-articles article{
+/* 	width: 300px;
+ */	height: 320px;
+	background-image: url("../img/art-img1.jpg");
+	background-size: cover;
+	width: 100%;
+   
+    /* background-image: url('img_flowers.jpg'); */
+    /* background-size: 100% 100%; */
+    /* border: 1px solid red; */
+
+}
+
+.MainIndex-articles article:nth-child(2){
+	background-image: url("../img/art-img2.jpg");
+}
+
+.MainIndex-articles article:nth-child(3){
+	background-image: url("../img/art-img3.jpg");
+}
+
--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+document.getElementById("defaultOpen").click();
+
+// ACCORDION
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("activee");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
+
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -24,3 +44,83 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " Slides-active";
 }
+
+
+
+
+
+// TABS
+
+
+
+
+
+function openItem(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("AsideMain-tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.target.className += " active";
+
+// event.target identifica el elemento en el que se ha producido el evento(red).
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MODAL
+
+
+var Modal = document.getElementsByClassName("Modal")[0];
+
+var btn = document.getElementsByClassName("opencontact")[0];
+
+var btn2 = document.getElementsByClassName("opencontact2")[0];
+
+var span = document.getElementsByClassName("Modal-close")[0];
+
+btn.onclick = function() {
+    Modal.style.display = "block";
+}
+btn2.onclick = function() {
+    Modal.style.display = "block";
+}
+
+span.onclick = function() {
+    Modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == Modal) {
+        Modal.style.display = "none";
+    }
+}
+
